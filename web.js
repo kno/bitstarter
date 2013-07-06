512cd28e1152bbbc163ee9ed0b262abd4c468891
kno <linsms@gmail.com>
2013-07-06 21:37:49 +0200

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var fs = require('fs')
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var fs = require('fs')
 fs.readFile('/etc/passwd', function (err, data) {
   if (err) throw err;
   response.send(data);
