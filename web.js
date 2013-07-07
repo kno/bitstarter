b446af81ae3ab6c83e4fd10461b638e8151f4d56
kno <linsms@gmail.com>
2013-07-07 08:24:37 +0200

    modified web.js

--- web.js
+++ web.js
@@ -3,11 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs')
-fs.readFileSync('./index.html', function (err, data) {
-  if (err) throw err;
-  response.send(data);
-});
+	var fs = require('fs')
+	var data = fs.readFileSync('index.html');
+	response.send(data.toString());
   
 });
 
